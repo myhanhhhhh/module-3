8fb2f5826961be354218dba703a70ac641a5d96f
Nguyen Thi My Hanh <131949097+myhanhhhhh@users.noreply.github.com>
2023-08-10 20:37:26 +0700

    up bai tap

--- example.sql
+++ example.sql
@@ -1,23 +1,31 @@
 create database student_management;
 use student_management;
 
+create table address(
+id_address int primary key auto_increment,
+ten_dia_chi varchar(45)
+);
+
 create table student(
 id_student int primary key auto_increment,
 name_student varchar(50),
-age_student int
+age_student int,
+country varchar(50),
+ma_dia_chi int,
+foreign key(ma_dia_chi) references address(id_adress)
 );
+
 insert into student(name_student, age_student)
 values
 ("Hanh", 23),
 ("Lien", 24);
+
 select * from student;
 delete from student
 where id_student = 1;
 
-set sql_safe_updates = 0;
-update name_student set name_student = "Sang" 
-where name_student = "Hanh";
-set sql_safe_updates = 1;
+update student set name_student = "Sang" 
+where id_student = 2;
 
 create table class(
 id_class int primary key auto_increment,
