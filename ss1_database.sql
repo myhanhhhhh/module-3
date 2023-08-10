5a02e990662287bc18e369e0c249661958f5f68d
Nguyen Thi My Hanh <131949097+myhanhhhhh@users.noreply.github.com>
2023-08-10 11:51:39 +0700

    up bai tap

---
+++ ss1_database.sql
@@ -0,0 +1,31 @@
+create database student_management;
+use student_management;
+create table student(
+id_student int primary key auto_increment,
+name_student varchar(50),
+age_student int
+);
+insert into student(name_student, age_student)
+values
+("Hanh", 23),
+("Lien", 24);
+select * from student;
+delete from student
+where id_student = 1;
+create table teacher(
+id_teacher int primary key auto_increment,
+name_teacher varchar(50),
+age_teacher int,
+country varchar(50)
+);
+insert into teacher(name_teacher, age_teacher, country)
+values
+("Viet", 30, "Da Nang"),
+("Thang", 30, "Da Nang"),
+("Chanh", 30, "Da Nang");
+select * from teacher;
+delete from teacher
+where id_teacher = 1;
+
+
+
